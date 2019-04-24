72836ca3475eeb4cc25f2cf4b43e17091aaf8e46
HARSH RAIZADA <43767314+harshraizada@users.noreply.github.com>
2019-04-24 16:17:43 -0400

    Update prep_main.py

--- prep_main.py
+++ prep_main.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     
     #load data
     its=pd.read_csv('Identify the sentiments.csv')
-    its.tweet = prep.sample_custom_prep(its.tweet)
+    its.tweet = prep.preprocessing(its.tweet)
     
