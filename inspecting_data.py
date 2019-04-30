d098992c2aa3f74e7b52b6defdf53a7361f910fa
HARSH RAIZADA <43767314+harshraizada@users.noreply.github.com>
2019-04-30 10:40:21 -0400

    Update inspecting_data.py

--- inspecting_data.py
+++ inspecting_data.py
@@ -13,3 +13,13 @@ def data_inspection(dataframe):
     #Exporing null values in datatset
     null=sns.heatmap(dataframe.isnull())
     return shape,columns,head,info,null
+
+
+def data_distribution(column_name):
+    """Distribution of label types"""
+    # check the number of positive vs. negative tagged sentences
+    positives ='number of positve tagged sentences is:  {}'.format(len(column_name[column_name == 0]))
+    negatives ='number of negative tagged sentences is: {}'.format(len(column_name[column_name == 1]))
+    #Percentage distribution of labels
+    percentage=column_name.value_counts(normalize='True')
+    return positives, negatives,percentage
