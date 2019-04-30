86952644e8b6d7dd1a75799f327672b5c5b91a0c
HARSH RAIZADA <43767314+harshraizada@users.noreply.github.com>
2019-04-29 21:36:51 -0400

    Update Importing_cleaning.py

--- Importing_cleaning.py
+++ Importing_cleaning.py
@@ -3,10 +3,6 @@ def import_clean(file_name):
     """Importing and cleaning of data"""
     #Reading files
     dataframe=pd.read_csv(file_name)
-    #Renaming columns.
-    dataframe.columns=['id','sentiment_type','tweet']
-    #Dropping null values
-    dataframe=dataframe.dropna(axis=1)
     #Dropping duplicates
     dataframe=dataframe.drop_duplicates()
     return dataframe
