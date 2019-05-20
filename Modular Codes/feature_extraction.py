534e8d0fcb28c6c46378123ed8fd098be7e142b4
harshraizada <43767314+harshraizada@users.noreply.github.com>
2019-05-20 10:12:40 -0400

    final

--- Modular Codes/feature_extraction.py
+++ Modular Codes/feature_extraction.py
@@ -1,23 +1,25 @@
- # Import necessary packages
- 
- from sklearn.model_selection import train_test_split
- from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
- 
- # Before feature extraction first splitting the data into train and test and then perform feature extraction
- 
- def traintest_split(X,y):
-    """Split into training and test (hold-out) set"""
-    # 80% of the input for training and 20% for training
-    Xtrain, Xtest, ytrain, ytest = train_test_split(X,y, test_size = 0.2,random_state=42,shuffle= True,stratify=y)
-    print (Xtrain.shape, ytrain.shape)
-    print (Xtest.shape, ytest.shape)
-    return Xtrain, Xtest, ytrain, ytest
-    
-    
- def ohe_vectorization(X_train,X_test):
-    """Vectorization (one-hot encoding)"""
-    cvohe = CountVectorizer(binary=True)
-    cvohe.fit(X_train)
-    Xohe_train = cvohe.transform(X_train)
-    Xohe_test = cvohe.transform(X_test)
-    return Xohe_train,Xohe_test
+# Import necessary packages
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+# Before feature extraction first splitting the data into train and test and then perform feature extraction
+
+
+def traintest_split(X, y):
+        """Split into training and test (hold-out) set"""
+        # 80% of the input for training and 20% for training
+        Xtrain, Xtest, ytrain, ytest = train_test_split(
+            X, y, test_size=0.2, random_state=42, shuffle=True, stratify=y)
+        print(Xtrain.shape, ytrain.shape)
+        print(Xtest.shape, ytest.shape)
+        return Xtrain, Xtest, ytrain, ytest
+
+
+def bow_vec(X_train, X_test):
+    """Vectorization with Bag of Words(one-hot encoding)"""
+    bowvec = CountVectorizer(binary=True)
+    bowvec.fit(X_train)
+    Xbow_train = bowvec.transform(X_train)
+    Xbow_test = bowvec.transform(X_test)
+    return Xbow_train, Xbow_test
--- Modular Codes/modelling.py
+++ Modular Codes/modelling.py
@@ -7,67 +7,80 @@ from sklearn.model_selection import cross_validate
 from sklearn.model_selection import RandomizedSearchCV
 import time
 from sklearn.metrics import make_scorer
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,classification_report,confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 
-def lr_modelling(X_train_vec,y_train,X_test_vec,y_test):
+
+def LRmodelling(X_train_vec, y_train, X_test_vec, y_test):
     """Applying Logistic Regression model"""
-    #Instantiate a logistic classifier
-    lr = LogisticRegression(random_state=42,class_weight='balanced')
+    # Instantiate a logistic classifier
+    lr = LogisticRegression(random_state=42, class_weight='balanced')
     lr.fit(X_train_vec, y_train)
-    y_predict_ohe=lr.predict(X_test_vec)
-    print('The accuracy of model is: \n',accuracy_score(y_test,y_predict_ohe))
-    print('The precision of model is: \n',precision_score(y_test,y_predict_ohe,pos_label=0))
-    print('The recall or sensitivity of model is: \n',recall_score(y_test,y_predict_ohe,pos_label=0))
-    f1score=f1_score(y_test,y_predict_ohe,pos_label=0) # calculating f1 score for positive label
-    print('The f1 score of model is %.2f' %(f1score))
-    print('The confusion matrix of model is: \n',confusion_matrix(y_test,y_predict_ohe))
-    print('The classification report of the model is: \n',classification_report(y_test,y_predict_ohe))
+    y_predict_bow = lr.predict(X_test_vec)
+    print('The accuracy of model is: \n', accuracy_score(y_test, y_predict_bow))
+    print('The precision of model is: \n', precision_score(y_test, y_predict_bow, pos_label=0))
+    print('The recall or sensitivity of model is: \n',
+          recall_score(y_test, y_predict_bow, pos_label=0))
+    # calculating f1 score for positive label
+    f1score = f1_score(y_test, y_predict_bow, pos_label=0)
+    print('The f1 score of model is %.2f' % (f1score))
+    print('The confusion matrix of model is: \n', confusion_matrix(y_test, y_predict_bow))
+    print('The classification report of the model is: \n',
+          classification_report(y_test, y_predict_bow))
     return
-    
-    
-def cross_validation(X_train_vec,y_train):
+
+
+def cross_validation(X_train_vec, y_train):
     # Build the stratified k-fold cross-validator(5-fold cross-validation) and defining metrices
     stratkfold = StratifiedKFold(n_splits=5, random_state=42)
-    scoring = ['accuracy', 'precision','recall','f1']
-    lr = LogisticRegression(random_state=42,class_weight='balanced')
-    #calculating score for the model after CV
-    result = cross_validate(lr, X_train_vec, y_train, cv=stratkfold, scoring=scoring,return_train_score=False)
-    print('The accuracy after CV is: \n',result['test_accuracy'].mean())
-    print('The precision after CV is: \n',result['test_precision'].mean())
-    print('The recall after CV is: \n',result['test_recall'].mean())
-    print('The f1 score after CV is: \n',result['test_f1'].mean())
+    scoring = ['accuracy', 'precision', 'recall', 'f1']
+    lr = LogisticRegression(random_state=42, class_weight='balanced')
+    # calculating score for the model after CV
+    result = cross_validate(lr, X_train_vec, y_train, cv=stratkfold,
+                            scoring=scoring, return_train_score=False)
+    print('The accuracy after CV is: \n', result['test_accuracy'].mean())
+    print('The precision after CV is: \n', result['test_precision'].mean())
+    print('The recall after CV is: \n', result['test_recall'].mean())
+    print('The f1 score after CV is: \n', result['test_f1'].mean())
     return
-    
-def tuning(X_train_vec,y_train):
-    """Modelling with random search (for hyperparameter tuning)"""
-    #Defining the grid values of the hyperparameters
-    dual=[True,False]
-    max_iter=[100,110,120,130,140,150]
-    C = [0.5,1.0,1.5,2.0,2.5]
-    param_grid = dict(dual=dual,max_iter=max_iter,C=C)
+
+
+# modelling with random search (for hyperparameter tuning)
+# Defining the grid values of the hyperparameters
+def tuning(X_train_vec, y_train):
+    """modelling with random search (for hyperparameter tuning)"""
+    # Defining the grid values of the hyperparameters
+    param_grid = {'penalty': ['l1', 'l2', 'elasticnet'], 'C': [
+        0.5, 1.0, 1.5, 2.0, 2.5], 'l1_ratio': [0, 0.25, 0.5, 0.75, 1]}
+    # Defining scoring metrices
+    scoring = {'accuracy': make_scorer(accuracy_score), 'precision': make_scorer(
+        precision_score), 'Recall': make_scorer(recall_score), 'F1 score': make_scorer(f1_score)}
+    # Instantiating LR model
+    lrht = LogisticRegression(max_iter=200, class_weight='balanced', solver='saga')
     # Running Randomized search on parameters
-    scoring = {'accuracy':make_scorer(accuracy_score), 'precision':make_scorer(precision_score),'Recall':make_scorer(recall_score),'F1 score':make_scorer(f1_score)}
-    lrht = LogisticRegression(penalty='l2',class_weight='balanced')
-    lrrs = RandomizedSearchCV(estimator=lrht, param_distributions=param_grid, cv = 5, n_iter=20, n_jobs=-1,random_state=42,scoring=scoring,refit='accuracy')
-    lrrs.fit(Xohe_train, ytrain)
+    lrrs = RandomizedSearchCV(estimator=lrht, param_distributions=param_grid, cv=5,
+                              n_iter=20, n_jobs=-1, random_state=42, scoring=scoring, refit='accuracy')
+    lrrs.fit(X_train_vec, y_train)
     print('Best Score: {}%'.format(lrrs.best_score_))
     print('Best Parameters: {}'.format(lrrs.best_params_))
     return
-    
-def tuned_params(X_train_vec,y_train,X_test_vec,y_test):
+
+
+def tuned_params(X_train_vec, y_train, X_test_vec, y_test):
     """LR with best parameters after hyperparameter tuning"""
-    lrbst=LogisticRegression(C=1.5,dual=False,max_iter=130,class_weight='balanced')
+    lrbst = LogisticRegression(max_iter=200, class_weight='balanced',
+                               solver='saga', C=1.5, penalty='l2', l1_ratio=0.25, random_state=42)
     lrbst.fit(X_train_vec, y_train)
-    y_predictbst_ohe=lrbst.predict(X_test_vec)
+    y_predictbst_bow = lrbst.predict(X_test_vec)
     # Model evaluation
-    ## Compute and print the confusion matrix and classification report
-    print('The accuracy of model is: \n',accuracy_score(y_test,y_predictbst_ohe))
-    print('The precision of model is: \n',precision_score(y_test,y_predictbst_ohe,pos_label=0))
-    print('The recall or sensitivity of model is: \n',recall_score(y_test,y_predictbst_ohe,pos_label=0))
-    f1score=f1_score(y_test,y_predictbst_ohe,pos_label=0) # calculating f1 score for positive label
-    print('The f1 score of model is %.2f' %(f1score))
-    print('The confusion matrix of model is: \n',confusion_matrix(y_test,y_predictbst_ohe))
-    print('The classification report of the model is: \n',classification_report(y_test,y_predictbst_ohe))
+    # Compute and print the confusion matrix and classification report
+    print('The accuracy of model is: \n', accuracy_score(y_test, y_predictbst_bow))
+    print('The precision of model is: \n', precision_score(y_test, y_predictbst_bow, pos_label=0))
+    print('The recall or sensitivity of model is: \n',
+          recall_score(y_test, y_predictbst_bow, pos_label=0))
+    # calculating f1 score for positive label
+    f1score = f1_score(y_test, y_predictbst_bow, pos_label=0)
+    print('The f1 score of model is %.2f' % (f1score))
+    print('The confusion matrix of model is: \n', confusion_matrix(y_test, y_predictbst_bow))
+    print('The classification report of the model is: \n',
+          classification_report(y_test, y_predictbst_bow))
     return
-    
-    
